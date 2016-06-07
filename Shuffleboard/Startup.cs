8364d227ea4b7cdd4a866df78d3becbe18ccf506
leighandrewtm <leighandrewtm@gmail.com>
2016-06-07 09:23:54 -0500

    Add project files.

---
+++ Shuffleboard/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Shuffleboard.Startup))]
+namespace Shuffleboard
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
